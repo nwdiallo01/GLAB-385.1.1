a101d0dd19b52f16cf0461c71b8d6f5ab55b08c7
nwdiallo01 <nwdiallo@gmail.com>
2026-09-04 12:17:06 -0400

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,13 @@
+# My name is Ndeye Diallo
+# This is my very first project in Python
+name = input("What's your name?")
+age = input("How old are you?")
+
+# Provide your email and mobile number
+email = input("What is your email?")
+mn = input("What is your mobile number?")
+
+print("Hello, " + name + "!")
+print("You are " + age + " years old.")
+print("Your email is " + email + ".")
+print("Your mobile number is " + mn + ".")
